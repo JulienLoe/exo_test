b82a8b8bf49375e32ab01876bc531b2cac5d66c0
JulienLoe <julien.loeuilleux62@gmail.com>
2023-08-24 22:38:38 +0200

    finish

---
+++ src/main/java/org/example/City.java
@@ -0,0 +1,52 @@
+package org.example;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class City {
+
+    public List<String> searchCity(String search){
+        List<String> listCity = new ArrayList<>();
+        listCity.add("Paris");
+        listCity.add("Budapest");
+        listCity.add("Skopje");
+        listCity.add("Rotterdam");
+        listCity.add("Valence");
+        listCity.add("Vancouver");
+        listCity.add("Amsterdam");
+        listCity.add("Vienne");
+        listCity.add("Sydney");
+        listCity.add("New York");
+        listCity.add("Londres");
+        listCity.add("Bangkok");
+        listCity.add("Hong Kong");
+        listCity.add("Dubaï");
+        listCity.add("Rome");
+        listCity.add("Istanbul");
+
+
+        int sizeSearch = search.length();
+        List<String> city = new ArrayList<>();
+        if (sizeSearch >2 || search.equals("*")) {
+            if (search == "*"){
+                for (String el : listCity){
+                    city.add(el);
+                }
+                return city;
+            }
+            for (int i = 0; i < listCity.size(); i++) {
+                if (listCity.get(i).length() >= sizeSearch && listCity.get(i).toLowerCase().substring(0, sizeSearch).equals(search.toLowerCase())) {
+                    city.add(listCity.get(i));
+                }
+            }
+        }
+            else{
+                throw new RuntimeException();
+            }
+        System.out.println(city.size());
+
+        return city;
+    }
+}
---
+++ src/test/java/org/example/CityTest.java
@@ -0,0 +1,58 @@
+package org.example;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class CityTest {
+
+    City city;
+    @Test
+    void testCityParis() {
+        //Arrange
+        city = new City();
+
+        //Act
+        List<String> result = city.searchCity("Paris");
+
+        //Assert
+        Assertions.assertEquals(Arrays.asList("Paris"), result);
+    }
+
+    @Test
+    void testCityAll() {
+        //Arrange
+        city = new City();
+
+        //Act
+        List<String> result = city.searchCity("*");
+        //Assert
+        Assertions.assertEquals(16, result.size());
+    }
+
+    @Test
+    void testCityFalseCity() {
+        //Arrange
+        city = new City();
+
+        //Act
+        List<String> result = city.searchCity("Lille");
+
+        //Assert
+        Assertions.assertTrue(result.isEmpty());
+    }
+
+    @Test
+    void testCityEmpty() {
+        //Arrange
+        city = new City();
+
+        //Act
+        List<String> result = city.searchCity("");
+
+        //Assert
+        Assertions.assertTrue(result.isEmpty());
+    }
+}
